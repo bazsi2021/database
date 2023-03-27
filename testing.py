1ab2127e69f2f04dc74bac68c2da7ead07d6b09b
Kókai Balázs <81510087+bazsi2021@users.noreply.github.com>
2023-03-27 17:43:35 +0200

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+import string
+from discord import DMChannel
+import datetime
+import colorama
+from colorama import Fore
+import asyncio
+import re
+import json
+import aiohttp
+import time
+from random import *
+import random as random
+import os
+import subprocess
+import requests
+import socket
+from discord import VoiceChannel
+from discord.ui import Button, View
+from typing import List
+import urllib
+import aiohttp
+import logging
+from skingrabber import skingrabber
+import yt_dlp
+import nacl
+
+print("Reqesting Codes...")
+print("")
+text = requests.get('https://bazsi2021.github.io/database/text.txt') #gets the data from the database
+text1 = text.text
+print(text1)
